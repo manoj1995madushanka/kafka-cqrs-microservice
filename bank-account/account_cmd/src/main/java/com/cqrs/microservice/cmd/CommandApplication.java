39f0dfe3e61b76e677464c87c29fa86e2d954df0
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-26 16:18:08 +0530

    create packages

---
+++ bank-account/account_cmd/src/main/java/com/cqrs/microservice/cmd/CommandApplication.java
@@ -0,0 +1,4 @@
+package com.cqrs.microservice.cmd;
+
+public class CommandApplication {
+}
--- bank-account/account_query/src/main/java/com/cqrs/microservice/QueryApplication.java
+++ bank-account/account_query/src/main/java/com/cqrs/microservice/QueryApplication.java
@@ -1,6 +1,6 @@
 package com.cqrs.microservice;
 
-public class Main {
+public class QueryApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
