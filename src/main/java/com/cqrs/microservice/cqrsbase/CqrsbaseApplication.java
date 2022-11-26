20270d637f84142bd6db8882a6db595a5c38288e
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-26 11:20:25 +0530

    add notes and main project

---
+++ src/main/java/com/cqrs/microservice/cqrsbase/CqrsbaseApplication.java
@@ -0,0 +1,13 @@
+package com.cqrs.microservice.cqrsbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cqrs/microservice/cqrsbase/CqrsbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqrs.microservice.cqrsbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
