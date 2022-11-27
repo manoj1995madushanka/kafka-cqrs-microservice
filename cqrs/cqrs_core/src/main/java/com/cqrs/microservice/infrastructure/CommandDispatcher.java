2fc50293d70b7e527461b16eb573866015b91bca
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-27 17:56:21 +0530

    add comments

--- cqrs/cqrs_core/src/main/java/com/cqrs/microservice/infrastructure/CommandDispatcher.java
+++ cqrs/cqrs_core/src/main/java/com/cqrs/microservice/infrastructure/CommandDispatcher.java
@@ -3,6 +3,9 @@ package com.cqrs.microservice.infrastructure;
 import com.cqrs.microservice.commands.BaseCommand;
 import com.cqrs.microservice.commands.CommandHandlerMethod;
 
+/*
+* this is mediator that manages the distribution of messages(command) among their objects
+* */
 public interface CommandDispatcher {
 
     <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
