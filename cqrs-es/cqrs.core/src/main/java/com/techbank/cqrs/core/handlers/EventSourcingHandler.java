11a1cb002caa5c4d2ab575c2256e7a829b1e0f7f
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-03 11:22:44 +0530

    add comment for note perpose of event sourcing handler

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/handlers/EventSourcingHandler.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/handlers/EventSourcingHandler.java
@@ -2,6 +2,10 @@ package com.techbank.cqrs.core.handlers;
 
 import com.techbank.cqrs.core.domain.AggregateRoot;
 
+/*
+ * this is responsible for retrieving all events for a given aggregate from the event
+ * store and to invoke the replayEvents method on aggregateRoot to recreate the latest state of the aggregate
+ * */
 public interface EventSourcingHandler<T> {
 
     void save(AggregateRoot aggregate);
